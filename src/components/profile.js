8d44febcfb8257a9c1bc7e34637b2669ad217c0a
David Mallard <correspondence@davidmallard.id.au>
2019-12-31 12:12:11 +1100

    feat: add user authentication

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import { useIdentityContext } from 'react-netlify-identity';
 
-const Profile = () => {
-  return (
+const Profile = ({ showModal }) => {
+  const identity = useIdentityContext();
+  const isLoggedIn = identity && identity.isLoggedIn;
+
+  const name = 
+    identity &&
+    identity.user &&
+    identity.user.user_metadata &&
+    identity.user.user_metadata.full_name;
+
+  return isLoggedIn && (
     <div className="dashboard-header">
       <nav>
         <Link to="/dashboard/secret" activeClassName="active">
@@ -12,7 +22,7 @@ const Profile = () => {
           See Your Base
         </Link>
       </nav>
-      <span>TODO show login status</span>
+      <span>Logged in as {name}. <button onClick={showModal}>log out</button></span>
     </div>
   )
 };
